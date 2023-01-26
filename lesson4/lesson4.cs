2ced1460d57ccee94da7bba4e74a22aab8b15ede
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:58:00 +0300

    Урок 4 задача 2

--- lesson4/lesson4.cs
+++ lesson4/lesson4.cs
@@ -3,13 +3,31 @@
 // Задача 1
 // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.Write("Введите число :");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите степень возведения :");
-int B = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for (int i = 0; i < B; i++)
+// Console.Write("Введите число :");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите степень возведения :");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for (int i = 0; i < B; i++)
+// {
+//     result = result * A;
+// }
+// Console.WriteLine($"{A} в степени {B} = {result}");
+
+// Задача 2 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Numsum(int num)
 {
-    result = result * A;
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + (num % 10);
+        num = num / 10;
+    }
+    return result;
 }
-Console.WriteLine($"{A} в степени {B} = {result}");
+
+Console.Write("Введите значение :");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел {num} = {Numsum(num)}");
