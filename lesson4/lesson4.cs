7bd2dcded6a762bde1e9e0518a81e2e78e5259cb
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:56:11 +0300

    Урок 4 задача 1

--- lesson4/lesson4.cs
+++ lesson4/lesson4.cs
@@ -1,1 +1,15 @@
 ﻿// ДОМАШНИЕ ЗАДАНИЯ 4 УРОКА
+
+// Задача 1
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число :");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень возведения :");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine($"{A} в степени {B} = {result}");
