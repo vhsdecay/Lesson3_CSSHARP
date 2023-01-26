1516aa0c3a16efb83cb8828bc95ef5564193b6c1
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:32:06 +0300

    Урок 2 задача 3

--- lesson2/lesson2.cs
+++ lesson2/lesson2.cs
@@ -12,19 +12,41 @@
 // Задача 2
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write("Введите значение: ");
+// Console.Write("Введите значение: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N >= 100)
+// {
+//     int result = N;
+//     while (result > 999)
+//     {
+//         result = result / 10;
+//     }
+//     result = result % 10;
+//     Console.WriteLine($"Третья цифра в числе {N} = {result} ");
+// }
+// else
+// {
+//     Console.WriteLine($"В числе {N} нет третьей цифры");
+// }
+
+// Задача 3
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите день недели: ");
 int N = Convert.ToInt32(Console.ReadLine());
-if (N >= 100)
+if (N <= 7 && N >= 1)
 {
-    int result = N;
-    while (result > 999)
+    if (N <6)
     {
-        result = result / 10;
+        Console.WriteLine($"{N} рабочий день в неделе");
     }
-    result = result % 10;
-    Console.WriteLine($"Третья цифра в числе {N} = {result} ");
+    else
+    {
+        Console.WriteLine($"{N} выходной день в неделе");
+    }
 }
 else
 {
-    Console.WriteLine($"В числе {N} нет третьей цифры");
+    Console.WriteLine($"{N} дня нет в неделе");
+
 }
