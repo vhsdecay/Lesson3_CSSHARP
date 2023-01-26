ad3a0d5e9789c3bc399add0f9746d0749190fe4b
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:59:51 +0300

    Урок 4 задача 3

--- lesson4/lesson4.cs
+++ lesson4/lesson4.cs
@@ -17,17 +17,35 @@
 // Задача 2 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-int Numsum(int num)
-{
-    int result = 0;
-    while (num > 0)
-    {
-        result = result + (num % 10);
-        num = num / 10;
-    }
-    return result;
-}
+// int Numsum(int num)
+// {
+//     int result = 0;
+//     while (num > 0)
+//     {
+//         result = result + (num % 10);
+//         num = num / 10;
+//     }
+//     return result;
+// }
 
-Console.Write("Введите значение :");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Сумма чисел {num} = {Numsum(num)}");
+// Console.Write("Введите значение :");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма чисел {num} = {Numsum(num)}");
+
+// Задача 3
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] array = new int[8];
+Random value = new Random();
+Console.Write($"Массив из 8 элементовв ->");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = value.Next(0, 100);
+    if (i == 7)
+    {
+        Console.Write($" {array[i]}");
+        break;
+    }
+    Console.Write($" {array[i]} ,");
+}
