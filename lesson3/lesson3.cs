9b26abf9bb273656204652699878d8647b97b992
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:45:40 +0300

    Урок 3 задача 2

--- lesson3/lesson3.cs
+++ lesson3/lesson3.cs
@@ -3,16 +3,36 @@
 // Задача 1 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.Write("Введите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num > 999 && num < 100000)
-{
-    if(num % 10 == num / 10000 % 10 && num / 10 % 10 == num / 1000 % 10)
-    {
-        Console.WriteLine($"Число {num} является палиндромом");
-    }
-    else
-        Console.WriteLine($"Число {num} не является палиндромом");
-}
-else
-    Console.WriteLine($"Число {num} не является пятизначным");
+// Console.Write("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 999 && num < 100000)
+// {
+//     if(num % 10 == num / 10000 % 10 && num / 10 % 10 == num / 1000 % 10)
+//     {
+//         Console.WriteLine($"Число {num} является палиндромом");
+//     }
+//     else
+//         Console.WriteLine($"Число {num} не является палиндромом");
+// }
+// else
+//     Console.WriteLine($"Число {num} не является пятизначным");
+
+// Задача 2
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты 1 точки");
+Console.Write("х :");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y :");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z :");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты 2 точки");
+Console.Write("х :");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y :");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z :");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+Console.WriteLine(Math.Round(result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)), 3));
