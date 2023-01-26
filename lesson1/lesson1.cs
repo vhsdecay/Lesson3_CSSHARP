dccff612665a37a411aa77a710f4c73f1be9a492
vhsdecay <vetvwhs@gmail.com>
2023-01-26 18:28:10 +0300

    Урок 1 задача 2

--- lesson1/lesson1.cs
+++ lesson1/lesson1.cs
@@ -3,15 +3,38 @@
 // Задача 1 
 // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+// Console.Write("Введите первое значение: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе значение: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+//     Console.Write($"Значение {a} больше {b}");
+// }
+// else
+// {
+//     Console.Write($"Ззначение {b} больше {a}");
+// }
+
+// Задача 2
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
 Console.Write("Введите первое значение: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе значение: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+Console.Write("Введите третие значение: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
 {
-    Console.Write($"Значение {a} больше {b}");
+    Console.Write($"Максимальное число {b}");
 }
-else
+else if (a < c)
 {
-    Console.Write($"Ззначение {b} больше {a}");
+    Console.Write($"Максимальное число {c}");
+}
+else 
+{
+    Console.Write($"Максимальное число {a}");
 }
