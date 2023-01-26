6caf89cbc89c1bd232abc36f7a9f98d06acdbc0b
vhsdecay <vetvwhs@gmail.com>
2023-01-26 18:59:44 +0300

    Урок 2 задача 2

--- lesson2/lesson2.cs
+++ lesson2/lesson2.cs
@@ -3,8 +3,28 @@
 // Задача 1  
 //  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите 3-х значное значенние: ");
+// Console.Write("Введите 3-х значное значенние: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int result = N % 100; 
+// result = result / 10;
+// Console.WriteLine($"Вторая цифра числа {N} = {result} ");
+
+// Задача 2
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите значение: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int result = N % 100; 
-result = result / 10;
-Console.WriteLine($"Вторая цифра числа {N} = {result} ");
+if (N >= 100)
+{
+    int result = N;
+    while (result > 999)
+    {
+        result = result / 10;
+    }
+    result = result % 10;
+    Console.WriteLine($"Третья цифра в числе {N} = {result} ");
+}
+else
+{
+    Console.WriteLine($"В числе {N} нет третьей цифры");
+}
