b1a0ceda3a6a393f55163362fd3efc8a43d69fd5
vhsdecay <vetvwhs@gmail.com>
2023-01-26 19:47:02 +0300

    Урок 3 задача 3

--- lesson3/lesson3.cs
+++ lesson3/lesson3.cs
@@ -20,19 +20,30 @@
 // Задача 2
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine("Введите координаты 1 точки");
-Console.Write("х :");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("y :");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("z :");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты 2 точки");
-Console.Write("х :");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("y :");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("z :");
-int z2 = Convert.ToInt32(Console.ReadLine());
-double result = 0;
-Console.WriteLine(Math.Round(result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)), 3));
+// Console.WriteLine("Введите координаты 1 точки");
+// Console.Write("х :");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y :");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("z :");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты 2 точки");
+// Console.Write("х :");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y :");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("z :");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double result = 0;
+// Console.WriteLine(Math.Round(result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)), 3));
+
+// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число :");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"3 -> "); 
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
