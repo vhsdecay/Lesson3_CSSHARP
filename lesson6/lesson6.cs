8cc96e4ee165ace03f79c07aa3f862195db835ec
vhsdecay <vetvwhs@gmail.com>
2023-01-29 20:16:50 +0300

    Урок 6 задача 1

--- lesson6/lesson6.cs
+++ lesson6/lesson6.cs
@@ -1,1 +1,32 @@
 ﻿// ДОМАШНИЕ ЗАДАНИЯ 6 УРОКА
+
+
+// Задача 1
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountInArrayMax0(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int[] NewMetod1(string numss)
+{
+    string[] arraynumsstring = numss.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+    int[] arraynumsint = arraynumsstring.Select(Int32.Parse).ToArray();
+    return arraynumsint; 
+}
+
+Console.Write($"Введите значения через пробел :");
+string numss = Console.ReadLine() ?? string.Empty;;
+int[] arraynumsint = NewMetod1(numss);
+Console.WriteLine($"Количество чисел больше 0 = {CountInArrayMax0(arraynumsint)}");
