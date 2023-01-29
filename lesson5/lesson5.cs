1ea0918395ec54fe15639eaa0b0da44260050f0c
vhsdecay <vetvwhs@gmail.com>
2023-01-29 18:01:22 +0300

    Урок 5 задача 1

--- lesson5/lesson5.cs
+++ lesson5/lesson5.cs
@@ -1,1 +1,47 @@
 ﻿// ДОМАШНИЕ ЗАДАНИЯ 5 УРОКА
+
+// Задача 1
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] RandomArray3()
+{
+    int[] array = new int[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintRandomArray(int[] randomarray3)
+{
+    Console.Write($"Рандомный массив трехзначных чисел : [");
+    for (int i = 0; i < randomarray3.Length; i++)
+    {
+        if (i != randomarray3.Length - 1)
+        {
+            Console.Write($"{randomarray3[i]}, ");
+        }
+        else Console.WriteLine($"{randomarray3[i]}]");
+    }
+}
+
+int[] randomarray3 = RandomArray3();
+PrintRandomArray(randomarray3);
+int countevennum = 0;
+for (int i = 0; i < randomarray3.Length; i++)
+{
+    while (randomarray3[i] != 0)
+    {
+        int evennum = randomarray3[i] % 10;
+        if (evennum % 2 == 0)
+        {
+            countevennum += 1;
+            randomarray3[i] /= 10;
+        }
+        else randomarray3[i] /= 10;
+           
+    }
+}
+Console.WriteLine($"Колличество чётных чисел в массиве = {countevennum}");
