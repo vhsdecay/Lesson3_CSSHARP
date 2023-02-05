6274b2f2d1e1f64bd4b47c86703ec121209d7403
vhsdecay <vetvwhs@gmail.com>
2023-02-05 19:00:37 +0300

    Урок 7 задача 1

--- lesson7/lesson7.cs
+++ lesson7/lesson7.cs
@@ -1,1 +1,25 @@
 ﻿// ДОМАШНИЕ ЗАДАНИЯ 7 УРОКА
+
+// Задача 1
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+double RandomNums()
+{
+    double nums = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 1);
+    return nums;
+}
+
+int m = 3;
+int n = 4;
+Console.WriteLine("Двумерный массив, случайных чисел: ");
+double[,] array = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = RandomNums();
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
