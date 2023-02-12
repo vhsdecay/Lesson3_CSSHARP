354ebeb6332fbb2b297df7e1542246abca031db3
vhsdecay <vetvwhs@gmail.com>
2023-02-12 16:43:00 +0300

    Урок 9 задача 3

--- lesson9/lesson9.cs
+++ lesson9/lesson9.cs
@@ -26,18 +26,45 @@
 // Задача 2
 // Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int SumNums(int M, int N)
+// int SumNums(int M, int N)
+// {
+//     if (N <= M)
+//         return N;
+//     else
+//     return N + SumNums(M, N - 1);
+// }
+
+// Console.Write("Введите значение начала отсчёта: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение конца отсчёта: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if (M < N) Console.Write($"Сумма {SumNums(M, N)}");
+// else Console.Write($"{M} меньше {N} (Ошибка)");
+
+
+// #
+// Задача 3
+//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int A(int m, int n)
 {
-    if (N <= M)
-        return N;
-    else
-    return N + SumNums(M, N - 1);
+    if (m == 0) 
+        return n + 1;
+
+    if (m != 0 && n == 0) 
+        return A(m - 1, 1);
+
+    if (m > 0 && n > 0) 
+        return A(m - 1, A(m, n - 1));
+        
+    return A(m, n);
 }
 
-Console.Write("Введите значение начала отсчёта: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение конца отсчёта: ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-if (M < N) Console.Write($"Сумма {SumNums(M, N)}");
-else Console.Write($"{M} меньше {N} (Ошибка)");
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(A(m, n));
