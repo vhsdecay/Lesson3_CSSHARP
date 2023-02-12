4b31da9ce47230ad026d98edca327e47976892d4
vhsdecay <vetvwhs@gmail.com>
2023-02-12 13:58:23 +0300

    Урок 9 задача 1

--- lesson9/lesson9.cs
+++ lesson9/lesson9.cs
@@ -1,1 +1,22 @@
 ﻿// ДОМАШНИЕ ЗАДАНИЯ 9 УРОКА
+
+// Задача 1
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void NaturalNums(int N)
+{
+    if (N == 1) 
+    {
+        Console.Write(N);
+    }
+    else
+    {
+        Console.Write($"{N}, ");
+        NaturalNums(N - 1);
+    }
+}
+
+Console.Write("Введите значение N:");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 1) NaturalNums(N);
+else Console.Write($"{N} меньше 1 (Ошибка)");
